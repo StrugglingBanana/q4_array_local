69295a2fb4c964a475720bc2ee93db8a925d3cb0
Tobias Classen <tclassen@wesleyan.edu>
2026-06-18 11:25:30 -0400

    Removed troubleshooting hardcoded portion.

--- main.cc
+++ main.cc
@@ -8,6 +8,8 @@
 #include "ActionInitialization.hh"
 
 #include "G4RunManagerFactory.hh"
+#include "G4MTRunManager.hh" // Ensure MT manager header is available
+#include "G4RunManager.hh"
 #include "G4SteppingVerbose.hh"
 #include "G4Version.hh"
 #include "G4UImanager.hh"
@@ -22,23 +24,21 @@ using namespace QArray;
 
 int main(int argc, char **argv)
 {
-  #ifdef G4MULTITHREADED
-  // If it's MT, check if it's crashing because of the GUI thread
-  auto* runManager = new G4RunManager; // Temporary force to Sequential for debugging
-  // auto* runManager = new G4MTRunManager; // Comment this out to test
+  // 1. Instantiate the CORRECT Multi-Threaded Run Manager
+#ifdef G4MULTITHREADED
+  auto* runManager = new G4MTRunManager; 
+  // Set the default number of threads (e.g., 4 or your Mac's core count)
+  runManager->SetNumberOfThreads(4);
 #else
   auto* runManager = new G4RunManager;
 #endif
-  // Detect interactive mode (if no arguments) and define UI session
-  //
+
+  // 2. Detect interactive mode (if no arguments) and define UI session
   G4UIExecutive *ui = nullptr;
   if (argc == 1)
   {
-    ui = new G4UIExecutive(argc, argv);
+    ui = new G4UIExecutive(argc, argv, "Qt"); // Explicitly pass "Qt" for macOS stability
   }
-
-  // Optionally: choose a different Random engine...
-  // G4Random::setTheEngine(new CLHEP::MTwistEngine);
 
   // use G4SteppingVerboseWithUnits
 #if G4VERSION_NUMBER >= 1100
@@ -46,14 +46,7 @@ int main(int argc, char **argv)
   G4SteppingVerbose::UseBestUnit(precision);
 #endif
 
-  // Construct the default run manager
-  //
-  // auto *runManager =
-  //     G4RunManagerFactory::CreateRunManager(G4RunManagerType::Default);
-
   // Set mandatory initialization classes
-  //
-  // Detector construction
   runManager->SetUserInitialization(new DetectorConstruction());
 
   // Physics list
@@ -62,21 +55,17 @@ int main(int argc, char **argv)
   physicsList->SetVerboseLevel(0);
   runManager->SetUserInitialization(physicsList);
 
-  // User action initialization
+  // User action initialization (Spawns worker local setups in MT mode!)
   runManager->SetUserInitialization(new ActionInitialization());
 
   // Initialize visualization
-  //
   G4VisManager *visManager = new G4VisExecutive;
-  // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
-  // G4VisManager* visManager = new G4VisExecutive("Quiet");
   visManager->Initialize();
 
   // Get the pointer to the User Interface manager
   G4UImanager *UImanager = G4UImanager::GetUIpointer();
 
   // Process macro or start UI session
-  //
   if (!ui)
   {
     // batch mode
@@ -93,10 +82,6 @@ int main(int argc, char **argv)
   }
 
   // Job termination
-  // Free the store: user actions, physics_list and detector_description are
-  // owned and deleted by the run manager, so they should not be deleted
-  // in the main() program !
-
   delete visManager;
   delete runManager;
 }
